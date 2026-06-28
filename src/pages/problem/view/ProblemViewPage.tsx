e211a6880966ec313ba25176aebaa27aca0b6e07
lee <li_yii@163.com>
2026-06-28 07:13:23 +0000

    删除题目页面引用小节列表处的图标

--- src/pages/problem/view/ProblemViewPage.tsx
+++ src/pages/problem/view/ProblemViewPage.tsx
@@ -457,7 +457,6 @@ let ProblemViewPage: React.FC<ProblemViewPageProps> = props => {
           className={style.referencedSection}
           href={`/t/${reference.trainingId}/${reference.chapterId}/${reference.sectionId}`}
         >
-          <Icon name="bookmark outline" />
           <span>{reference.sectionTitle}</span>
         </Menu.Item>
       ))}
